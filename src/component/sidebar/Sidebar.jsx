5e23103dbb99cad2e91d10d76a6abc35680294f8
Fonyuy Gita <FonyuyJudegita@gmail.com>
2023-02-24 13:34:54 -0800

    third commit today

--- src/component/sidebar/Sidebar.jsx
+++ src/component/sidebar/Sidebar.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 import "./Sidebar.scss"
 const Sidebar = () => {
   return (
-    <div className='sidebar'>Sidebar</div>
+    <div className='sidebar'>
+    <div className="top">
+    <span className="logo">GitAdmin<a>X</a></span>
+    </div>
+    <hr/>
+
+    <div className="center">
+    <ul>
+    
+    <li><span>Dashboard</span></li>
+    <li><span>Dashboard</span></li>
+    <li><span>Dashboard</span></li>
+    <li><span>Dashboard</span></li>
+    <li><span>Dashboard</span></li>
+    <li><span>Dashboard</span></li>
+
+    </ul>
+    <div className="bottom">Color option</div>
+    </div>
+    </div>
   )
 }
 
