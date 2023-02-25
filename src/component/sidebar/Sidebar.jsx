9edafacf739c0d621b5dfc47ae6fd4017f821690
Fonyuy Gita <FonyuyJudegita@gmail.com>
2023-02-24 19:27:42 -0800

    fourth commit today

--- src/component/sidebar/Sidebar.jsx
+++ src/component/sidebar/Sidebar.jsx
@@ -1,5 +1,21 @@
 import React from 'react'
 import "./Sidebar.scss"
+// our icons goes here
+
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import EditNotificationsIcon from '@mui/icons-material/EditNotifications';
+import SystemSecurityUpdateGoodIcon from '@mui/icons-material/SystemSecurityUpdateGood';
+import SyncAltIcon from '@mui/icons-material/SyncAlt';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import LogoutIcon from '@mui/icons-material/Logout';
+import SettingsApplicationsIcon from '@mui/icons-material/SettingsApplications';
+import GroupIcon from '@mui/icons-material/Group';
+import ProductionQuantityLimitsIcon from '@mui/icons-material/ProductionQuantityLimits';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import QueryStatsIcon from '@mui/icons-material/QueryStats';
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import SyncAlt from '@mui/icons-material/SyncAlt';
+
 const Sidebar = () => {
   return (
     <div className='sidebar'>
@@ -11,12 +27,55 @@ const Sidebar = () => {
     <div className="center">
     <ul>
     
-    <li><span>Dashboard</span></li>
-    <li><span>Dashboard</span></li>
-    <li><span>Dashboard</span></li>
-    <li><span>Dashboard</span></li>
-    <li><span>Dashboard</span></li>
-    <li><span>Dashboard</span></li>
+    <li>
+    <DashboardIcon/>
+    <span>Dashboard</span></li>
+    <li>
+    <GroupIcon/>
+    <span>Users</span>
+    </li>
+    <li>
+    <ProductionQuantityLimitsIcon/>
+    <span>Product</span>
+    </li>
+    <li>
+    <BookmarkBorderIcon/>
+    <span>Order</span>
+    </li>
+    <li>
+    <LocalShippingIcon/> 
+    <span>Delivery</span>
+    </li>
+    <li>
+    <QueryStatsIcon/> 
+    <span>Stats</span>
+    </li>
+    <li>
+    <EditNotificationsIcon/>
+    <span>Notification</span>
+    </li>
+    <li>
+< SystemSecurityUpdateGoodIcon />
+    <span>System health</span>
+    
+    </li>
+    <li>
+ <SyncAltIcon/>
+    <span>Logs</span>
+    </li>
+    <li>
+    <SettingsApplicationsIcon/>
+    <span>Settings</span>
+    </li>
+    <li>
+    <AccountBoxIcon/>
+    <span>Profile</span>
+    </li>
+    <li>
+    <LogoutIcon/>   
+     <span>Logout</span>
+    </li>
+    
 
     </ul>
     <div className="bottom">Color option</div>
