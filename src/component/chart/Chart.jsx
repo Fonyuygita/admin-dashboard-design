4fc9014cc4ea911a0c42773a85f583a61751a618
Fonyuy Gita <FonyuyJudegita@gmail.com>
2023-02-25 17:09:35 -0800

    final commit for todat

--- src/component/chart/Chart.jsx
+++ src/component/chart/Chart.jsx
@@ -1,10 +1,93 @@
-import React from 'react'
-import "./Chart.scss"
+import React from "react";
+import "./Chart.scss";
+// import rechart to use here
+
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 
 const Chart = () => {
-  return (
-    <div className='chart'>Chart</div>
-  )
-}
+  // data goes here
 
-export default Chart
+  const data = [
+    
+    {
+      "name": "January",
+      "Total": 2780,
+      
+    },
+    {
+      "name": "February",
+      "Total": 1890,
+      
+    },
+    {
+      "name": "March",
+      "Total": 2390,
+      
+    },
+    {
+      "name": "May",
+      "Total": 3490,
+      
+    }
+  ]
+  
+
+//   const data = [
+//     { name: "January", Total: 1234 },
+//     { name: "February", Total: 334 },
+//     { name: "March", Total: 120 },
+//     { name: "April", Total: 340 },
+//     { name: "May", Total: 100 },
+//     { name: "June", total: 140 },
+//   ];
+
+
+
+
+  return (
+    <div className="chart">
+    <div className="title">Here You can visualize the data</div>
+      
+      <ResponsiveContainer width="100%" aspect={2/1}>
+        <AreaChart
+          width={730}
+          height={250}
+          data={data}
+          margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+        >
+          <defs>
+            <linearGradient id="total" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+            </linearGradient>
+           
+          </defs>
+          <XAxis dataKey="name" name="Month" stroke="#666"/>
+          <YAxis />
+          <CartesianGrid strokeDasharray="3 3"  className="chartGrid"/>
+          <Tooltip />
+          <Area
+            type="monotone"
+            dataKey="Total"
+            stroke="#8884d8"
+            fillOpacity={1}
+            fill="url(#total)"
+          />
+         
+        </AreaChart>
+      </ResponsiveContainer>
+
+    
+    </div>
+  );
+};
+
+export default Chart;
